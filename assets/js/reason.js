c5b80f1d3a4d5f165621fbfcf60cf1b73e5544fb
eri <b115340@gmail.com>
2025-11-29 21:03:59 +0900

    セクションタイトルの動きがそれぞれ出るようにjsを修正

--- assets/js/reason.js
+++ assets/js/reason.js
@@ -1,3 +1,4 @@
+/*それぞれ開いたら説明画面が出る*/
 $(function () {
   $(".grid-item").on("click", function () {
     const target = $(this).data("target");
@@ -11,20 +12,24 @@ $(function () {
   });
 });
 
-
+/*スクロールしたらセクションタイトルがフェードインする*/
 $(function(){
-  const $target =$(".title-box.js-scroll-trigger");
+  const $targets =$(".title-box.js-scroll-trigger");
 
   function checkAnimation(){
-    const targetPos = $target.offset().top;
     const scroll = $(window).scrollTop();
     const windowHeight = $(window).height();
+
+    $targets.each(function(){
+      const $target = $(this);
+      const targetPos = $target.offset().top;
 
     if(scroll > targetPos - windowHeight * 0.8) {
       if(!$target.hasClass("is-animated")){
         $target.addClass("is-animated");
       }
     }
+  });
   }
 
   $(window).on("scroll",checkAnimation);
