e2df6568591b963a50906f719ce572bdaeb6b97e
eri <b115340@gmail.com>
2025-11-30 21:34:22 +0900

    カウンセリング誘導の細かな修正

--- assets/js/counseling.js
+++ assets/js/counseling.js
@@ -1,15 +1,15 @@
 
 const tab = document.querySelector('.g__counseling__tab');
-const empathy = document.querySelector('#empathy');
+const worry = document.querySelector('.p-empathy__worries__wrapper');
 
 function toggleClassByWidth() {
 
   //768px以下のときにだけ制御
   if (window.innerWidth <= 768) {
 
-    const empathyBottom = empathy.offsetTop + empathy.offsetHeight;
+    const worryBottom = worry.offsetTop + worry.offsetHeight;
 
-    if (window.scrollY >= empathyBottom) {
+    if (window.scrollY >= worryBottom) {
       tab.classList.add('g__counseling__sp');
     } else {
       tab.classList.remove('g__counseling__sp');
